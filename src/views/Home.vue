437e02f29a60fdbce03da956b069255f8a5077ac
Ray C <rayc2045@gmail.com>
2021-05-10 14:02:15 +0800

    Upload files

---
+++ src/views/Home.vue
@@ -0,0 +1,50 @@
+<script>
+import { reactive, toRefs } from 'vue';
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {},
+  setup() {
+    const state = reactive({
+      pokemons: [],
+      urlIdLookop: {},
+    });
+
+    fetch('https://pokeapi.co/api/v2/pokemon?offset=0')
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        state.pokemons = data.results;
+        state.urlIdLookop = data.results.reduce(
+          (acc, cur, idx) => (acc = { ...acc, [cur.name]: idx + 1 }),
+          {}
+        );
+      });
+
+    return {
+      ...toRefs(state),
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="w-full flex justify-center">
+    <input
+      class="mt-10 p-2 border-blue-500 border-2"
+      type="text"
+      placeholder="Enter Pokemon here..."
+    />
+  </div>
+
+  <div class="mt-10 p-4 flex flex-wrap justify-center">
+    <div
+      class="ml-4 text-2x text-blue-500"
+      v-for="(pokemon, idx) in pokemons"
+      :key="idx"
+    >
+      {{ pokemon.name }}
+    </div>
+  </div>
+</template>
